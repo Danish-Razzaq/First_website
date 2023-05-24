23b77c780f93a440f4ee568740646dcd0c3ad6dc
Muhammad Danish <125806096+Danish-Razzaq@users.noreply.github.com>
2023-05-24 16:05:44 +0500

    Add files via upload

--- src/app/data/page.tsx
+++ src/app/data/page.tsx
@@ -1,21 +1,20 @@
-import { Numans } from "next/font/google";
-import Link from "next/link";
+import React from 'react';
+import { Numans } from 'next/font/google';
+import Link from 'next/link';
+
 const Data = async () => {
+  const api = await fetch(`https://jsonplaceholder.typicode.com/posts`);
+  const res = await api.json();
 
-    const api = await fetch(`https://jsonplaceholder.typicode.com/posts`)
-    const res = await api.json()
-    // console.log(res);
-
-    return (
-        <div className="md:p-5 lg:p-5 p-2" >
-            {res.map((item:any ,id:any) => {
-                return (
-                  <li>  <Link href={`/data/${item.id}`}>{`${item.title}`}</Link></li>
-                )
-            })}
-        </div>
-    )
+  return (
+    <div className="md:p-5 lg:p-5 p-2">
+      {res.map((item: any) => (
+        <li key={item.id}>
+          <Link href={`/data/${item.id}`}>{item.title}</Link>
+        </li>
+      ))}
+    </div>
+  );
 };
-
 
 export default Data;
